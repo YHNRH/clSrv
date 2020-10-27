1e9ccd42ed9cde9b4d57ca0eb3471fdcb14b5a5d
Ilya Nikitin <f1uctus@mail.ru>
2020-10-27 12:07:03 +0300

    Java code reformat
    According to common standards

--- client/Client.java
+++ client/Client.java
@@ -1,137 +1,107 @@
 // DEP == DEPRECATED
 
-import java.util.*;
-import java.awt.event.*;
-import javax.swing.*;
-import java.awt.*;
-import java.nio.charset.StandardCharsets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
 import java.net.Socket;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
-import java.io.PrintWriter;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
+import java.util.Date;
 
-public class Client
-{
-    public static void main(String[] a)
-    {
-	MyFrame frame = new MyFrame();
-       
-	//   	Scanner scaner  = new Scanner(System.in);       DEP
+public class Client {
+    public static void main(String[] a) {
+        MyFrame frame = new MyFrame();
 
-	String  reply   = "ERROR";
-	//	System.out.println("Enter your name");          DEP
-	//      String name = scaner.nextLine();                DEP
-	String name = frame.getName();
-	
-	try (
-	     Socket socket  = new Socket("localhost", 8080);
+        // Scanner scaner  = new Scanner(System.in);       DEP
 
-	     )
-	    {
-		
-		try (
-		     DataInputStream input   = new DataInputStream(socket.getInputStream());
-		     DataOutputStream output = new DataOutputStream(socket.getOutputStream())) {
+        // String reply = "ERROR";
+        // System.out.println("Enter your name");          DEP
+        // String name = scaner.nextLine();                DEP
+        String name = frame.getName();
 
-		    Thread thread = new Thread(() ->
-					       {
-						   while(true)
-						       {
-							   try{
-							       frame.getMainArea().printMessage(input.readUTF());
-							  
-							   }
-							   catch(Exception e)
-							       {
-								   System.out.println("ERRORRRRR");
-							       }
+        try (
+            Socket socket = new Socket("localhost", 8080)
+        ) {
+            try (
+                DataInputStream input = new DataInputStream(socket.getInputStream());
+                DataOutputStream output = new DataOutputStream(socket.getOutputStream())) {
 
-							   try
-							       {
-								   Thread.sleep(500);
-							       }
-							   catch(Exception e)
-							       {}
-						       }
-					       });
-		    
-		    output.writeUTF(name);
-		    output.flush();
-		    //System.out.println("You're connected!\nPrint help to show all commands");
-		    		    thread.start();
-		     
-		    
-		   
-		    
-		    frame.getBotPanel().getButton().addActionListener(new ActionListener() {
-			    public void actionPerformed(ActionEvent e) {
-				Date date = new Date();
-				try
-				    {
-					String request =  frame.getBotPanel().getTextField().getText();
-					frame.getBotPanel().getTextField().setText("");
-					output.writeUTF(date + " "+ name +"> " + request);
-					output.flush();
-				    }
-				catch (Exception ex)
-				    {
-					
-				    }
-			    }
-			});
-		    
-		    // while (!request.equals("quit"))
-		    //	{
-			    //     reply = "ERROR";
-			    //	   System.out.print(name +"> "); DEP
-			    			
-			    //     request = scaner.nextLine();
-			    //	   output.writeUTF(request);
-			    //     output.flush();
+                Thread thread = new Thread(() ->
+                {
+                    while (true) {
+                        try {
+                            frame.getMainArea().printMessage(input.readUTF());
 
-			    //	    reply = input.readUTF();
+                        } catch (Exception e) {
+                            System.out.println("ERRORRRRR");
+                        }
 
-			    //      if (reply.equals("---- You're entered chat room ----"))
-			    //	{
-			    //	    System.out.println("---- You're entered chat room ----\nPrint /quit to left");
-		    //			    frame.getMainArea().printMessage("---- You're entered chat room ----\nPrint /quit to left");
-		
-				    // while(!request.equals("/quit"))
-				    //	{
-					    
-					   
+                        try {
+                            Thread.sleep(500);
+                        } catch (Exception e) {
+                        }
+                    }
+                });
 
-				    //	}
-				    //thread.stop();
-				    //reply = input.readUTF();
-				    //	}
+                output.writeUTF(name);
+                output.flush();
+                // System.out.println("You're connected!\nPrint help to show all commands");
+                thread.start();
 
-				    //System.out.println(reply);
-				    //	}
 
-		    while(true)
-			{
-			    frame.getMainArea().repaint();
-			    try
-				{
-				    Thread.sleep(150);
-				}
-			    catch (Exception e)
-				{}
-			}
+                frame.getBotPanel().getButton().addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        Date date = new Date();
+                        try {
+                            String request = frame.getBotPanel().getTextField().getText();
+                            frame.getBotPanel().getTextField().setText("");
+                            output.writeUTF(date + " " + name + "> " + request);
+                            output.flush();
+                        } catch (Exception ex) {
+
+                        }
+                    }
+                });
+
+//				while (!request.equals("quit")) {
+//					reply = "ERROR";
+//					System.out.print(name + "> "); // DEP
+//
+//					request = scaner.nextLine();
+//					output.writeUTF(request);
+//					output.flush();
+//
+//					reply = input.readUTF();
+//
+//					if (reply.equals("---- You're entered chat room ----")) {
+//						System.out.println(
+//						    "---- You're entered chat room ----\nPrint /quit to left"
+//						);
+//						frame.getMainArea().printMessage(
+//						    "---- You're entered chat room ----\nPrint /quit to left"
+//					    );
+//
+//						while (!request.equals("/quit")) {
+//						}
+//						thread.stop();
+//						reply = input.readUTF();
+//					}
+//
+//					System.out.println(reply);
+//				}
+
+                while (true) {
+                    frame.getMainArea().repaint();
+                    try {
+                        Thread.sleep(150);
+                    } catch (Exception e) {
+                    }
                 }
-		 catch (IOException ex) {
-		     ex.printStackTrace();
-        }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
 
-	    
-	    }
-	catch (IOException ex) {
-	    System.out.println("Cannot connect to server");
-	}
+
+        } catch (IOException ex) {
+            System.out.println("Cannot connect to server");
+        }
     }
 }
--- client/TopMenu.java
+++ client/TopMenu.java
@@ -1,20 +1,17 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class TopMenu extends JMenuBar
-{
+public class TopMenu extends JMenuBar {
     JMenu m1 = new JMenu("FILE");
     JMenu m2 = new JMenu("Help");
     JMenuItem m11 = new JMenuItem("Open");
     JMenuItem m22 = new JMenuItem("Save as");
-   
-    
-    public void paintComponent(Graphics g)
-    {
-	super.paintComponent(g);
-	m1.add(m11);
-	m1.add(m22);
-	this.add(m1);
-	this.add(m2);
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        m1.add(m11);
+        m1.add(m22);
+        this.add(m1);
+        this.add(m2);
     }
 }
